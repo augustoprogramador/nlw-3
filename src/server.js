2f65db2c433e3e19725972af0817458713a75b1a
augustoprogramador <augustoprogramador@hotmail.com>
2020-10-15 17:39:45 -0300

    Instalação do express e nodemon e criação do servidor

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// importar dependência
+const express = require('express')
+// iniciando o express
+const server = express()
+
+// criar uma rota
+server.get('/', () => {
+    
+})
+
+// ligar o servidor
+server.listen(5500)
