36f74f6e080019da51da59ae1b9d9754b98d965a
augustoprogramador <augustoprogramador@hotmail.com>
2020-10-15 19:01:04 -0300

    alteração da extensão html para HBS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 // importar dependência
 const express = require('express')
+const path = require('path')
+
 // iniciando o express
 const server = express()
+server
+    // utilizando os arquivos estáticos
+    .use(express.static('public'))
 
-// criar uma rota
-server.get('/', () => {
-    
-})
+    // configurar template engine
+    .set('views', path.join(__dirname, "view"))
+    .set('view engine', 'hbs')
+
+    // criar uma rota
+    .get('/', (request, response) => {
+        return response.render('index')
+    })
 
 // ligar o servidor
 server.listen(5500)
