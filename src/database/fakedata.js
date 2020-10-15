6ae5ad63cc686638a31fc5167334d9e93fdddf85
augustoprogramador <augustoprogramador@hotmail.com>
2020-10-15 20:38:41 -0300

    Definição de rotas

---
+++ src/database/fakedata.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Lar das meninas",
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1600712243809-7a3dd4e68fff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1595009503377-e3be116106b6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+
+
+            //https://source.unsplash.com/random?id=2
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opnening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos",
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1600712243809-7a3dd4e68fff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1595009503377-e3be116106b6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+
+
+            //https://source.unsplash.com/random?id=2
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opnening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
